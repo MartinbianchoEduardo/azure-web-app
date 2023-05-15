1cf6428bbaf67538a0a3682759785115e6903626
MartinbianchoEduardo <martinbianchoeduardo@gmail.com>
2023-05-15 15:19:25 -0300

    add first razor page

---
+++ Pages/Index2.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace azure_web_app.Pages
+{
+    public class Index2 : PageModel
+    {
+        public string Message { get; private set; } = "PageModel in C#";
+
+        public void OnGet()
+        {
+            Message += $" Server time is { DateTime.Now} ";
+        }
+    }
+}
