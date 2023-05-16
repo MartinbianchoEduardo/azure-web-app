c7f01030ffcf7af03e00c8d264123c00e0aa2ac2
MartinbianchoEduardo <martinbianchoeduardo@gmail.com>
2023-05-16 09:14:05 -0300

    add connection to in memory db

---
+++ Models/MyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace azure_web_app.Models
+{
+    public class MyDbContext : DbContext
+    {
+
+        public DbSet<Person> People { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("MyDb");
+        }
+
+    }
+}
---
+++ Pages/People.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace azure_web_app.Pages
+{
+    public class PeopleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
